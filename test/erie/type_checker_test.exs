3d13b0ada3ec2d4d998e388f0d3036a01292e269
Erik Person <eperson6@gmail.com>
2020-01-12 10:28:12 -0600

    Tuple and List signature parsing.

--- test/erie/type_checker_test.exs
+++ test/erie/type_checker_test.exs
@@ -2,32 +2,6 @@ defmodule Erie.TypeCheckerTest do
   use ExUnit.Case
   alias Erie.{Parser, Translator, TypeChecker}
 
-  describe "signature parsing" do
-    test "basic types" do
-      code = """
-      (sig one [] Integer)
-      (def one [] 1)
-      (sig two [String String] String)
-      (def two [x y] y)
-      """
-
-      {:ok, forms} = Parser.parse(code)
-      translator = Translator.from_parsed(forms, {:Core, 1}, false)
-
-      assert translator.signatures == [
-               {:one, {[], :Integer}},
-               {:two, {[:String, :String], :String}}
-             ]
-    end
-
-    test "polymorphic types" do
-      assert "get to this later"
-    end
-  end
-
-  test "inner function call checks" do
-  end
-
   describe "parameter checks" do
     test "basic function" do
       code = """
